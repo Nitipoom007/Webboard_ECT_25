b789783ce16fb1ada56ec8e2c95c31331c06b35f
Nitipoom007 <s6603052421160@email.kmutnb.ac.th>
2024-03-06 20:10:47 +0700

    SQL

--- webboard.sql
+++ webboard.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2024 at 11:10 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Mar 06, 2024 at 02:10 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,11 +49,20 @@ INSERT INTO `category` (`id`, `name`) VALUES
 
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL,
-  `content` varchar(20148) NOT NULL,
+  `content` mediumtext NOT NULL,
   `post_date` datetime NOT NULL,
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `content`, `post_date`, `user_id`, `post_id`) VALUES
+(1, 'เม้นแรกคับ', '2024-03-06 20:03:02', 6, 2),
+(2, 'เห็นด้วยคับมวยไทยชนะใสๆ เขมรอ่อนแอมากๆ ', '2024-03-06 20:07:16', 7, 2),
+(3, 'F แน่นอนคับ', '2024-03-06 20:07:32', 7, 1);
 
 -- --------------------------------------------------------
 
@@ -76,7 +85,7 @@ CREATE TABLE `post` (
 
 INSERT INTO `post` (`id`, `title`, `content`, `post_date`, `cat_id`, `user_id`) VALUES
 (1, 'จะ F มั้ยครับ', 'ผมเอาแต่ลอกงานเพื่อน ผมจะ F มั้ยครับ', '2024-02-28 16:58:07', 2, 6),
-(2, 'มวยไทย', 'คนต่อยกัน', '2024-02-28 17:02:55', 3, 6);
+(2, 'มวยไทย VS กุมขแมร์', 'มวยไทยชนะใสๆ', '2024-02-28 17:02:55', 3, 6);
 
 -- --------------------------------------------------------
 
@@ -99,7 +108,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `login`, `password`, `name`, `gender`, `email`, `role`) VALUES
-(6, 'admin', '8dc9fa69ec51046b4472bb512e292d959edd2aef', 'adminza', 'm', 'adminstator@gmail.com', 'm');
+(6, 'admin', '8dc9fa69ec51046b4472bb512e292d959edd2aef', 'adminza', 'm', 'adminstator@gmail.com', 'm'),
+(7, 'Prasitthichoke', 'c9deba1e15cbdddeaa9b714a59af5ce192717ff0', 'ประสิทธิโชค', 'm', 'Prasit123@gmail.com', 'm');
 
 --
 -- Indexes for dumped tables
@@ -109,6 +119,12 @@ INSERT INTO `user` (`id`, `login`, `password`, `name`, `gender`, `email`, `role`
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -134,6 +150,12 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
@@ -143,7 +165,7 @@ ALTER TABLE `post`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
